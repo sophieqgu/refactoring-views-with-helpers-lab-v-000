dfefe28ea1c8ccab8ff701f987bda1b4f85fd540
Sophie Gu <sophieqgu@gmail.com>
2020-12-18 05:14:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    @song = Song.find(params[:id])
+    @song.artist
   end
 
   def artist_name=(name)
+    @song = Song.find(params[:id])
+    @song.artist.build(:name => name)
+    @song.save
   end
 end
