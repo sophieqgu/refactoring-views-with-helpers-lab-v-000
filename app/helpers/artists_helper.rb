b214322da4b83e23f99f45915f050a31ac69e317
Sophie Gu <sophieqgu@gmail.com>
2020-12-18 05:19:50 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
   def display_artist(song)
-    if song.artist.exist?
+    if song.artist.exists?
       redirect_to artist_path(song.artist)
     else
       redirect_to edit_song_path(song)
-    end 
+    end
   end
 end
